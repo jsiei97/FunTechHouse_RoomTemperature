265ed314861425ddb3daffdb35e2b703695220b2
Johan Simonsson <github@fun-tech.se>
2012-06-13 20:55:15 +0200

    Variable overflow at raw 100, and some cleanup

--- FunTechHouse_RoomTemperature.ino
+++ FunTechHouse_RoomTemperature.ino
@@ -1,6 +1,5 @@
 #include <SPI.h>
 #include <Ethernet.h>
-//#include <PubSubClient.h>
 #include "PubSubClient.h"
 
 // Update these with values suitable for your network.
@@ -8,51 +7,50 @@ byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0x01 };
 byte ip[]     = { 192, 168, 0, 31 };
 byte server[] = { 192, 168, 0, 64 };
 
+char project_name[]  = "FunTechHouse_RoomTemperature";
+char topic_out[] = "FunTechHouse/Room1/Temperature";
+
+PubSubClient client(server, 1883, callback);
 
 void callback(char* topic, byte* payload,unsigned int length) 
 {
     // handle message arrived
 }
 
-PubSubClient client(server, 1883, callback);
-
 void setup()
 {
     analogReference(EXTERNAL); //3.3V
 
     Ethernet.begin(mac, ip);
-    if (client.connect("FunTechHouse_RoomTemperature")) 
+    if (client.connect(project_name)) 
     {
-        client.publish("FunTechHouse/Room1/temperature","#hello world");
+        client.publish(topic_out, "#Hello world");
         //client.subscribe("inTopic");
     }
 }
 
-//uint8_t miss_cnt = 0;
-
 void loop()
 {
-    int reading = analogRead(A2);  
-    float temperature = reading * 330; //Aref 3,3V 10mV/C
-    temperature /= 1024.0; 
+    int reading = analogRead(A2);
+    double temperature = reading * 3.30; // 3.3V Aref 
+    temperature /= 1024.0;               // ADC resolution
+    temperature *= 100;                  // 10mV/C (0.01V/C)
 
     if(client.loop() == false)
     {
-        client.connect("FunTechHouse_RoomTemperature");
-        //miss_cnt++;
+        client.connect(project_name);
     }
 
-    char str[30];
+    char str[40];
     int temp_hel = (int)(temperature);
     int temp_del = (int)((temperature-temp_hel)*10);
 
-    snprintf(str, 30, "temperature=%d.%d ; raw=%04d", temp_hel, temp_del, reading);
+    snprintf(str, 40, "temperature=%d.%d ; raw=%04d", temp_hel, temp_del, reading);
 
     if(client.connected())
     {
-        client.publish("FunTechHouse/Room1/temperature", str);
+        client.publish(topic_out, str);
     }
 
     delay(10000); 
 }
-
